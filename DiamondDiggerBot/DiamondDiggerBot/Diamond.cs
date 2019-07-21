be41f298a272070cf46f9124fbff526c10ea3484
alekhawkins17 <alekhawkins17@gmail.com>
2019-07-20 20:58:37 -0700

    Started Diamond Class. Added Color detection. Still need to add support for other colors.

---
+++ DiamondDiggerBot/DiamondDiggerBot/Diamond.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace DiamondDiggerBot
+{
+	class Diamond
+	{
+		// Properties
+		public DiamondColor myColor = DiamondColor.UNKNOWN;
+		public bool isGroundBlock = false;
+
+		// Constructors
+		public Diamond(Color inputColor) {
+			if (inputColor == PURPLE_COLOR)
+			{
+				myColor = DiamondColor.PURPLE;
+			}
+		}
+
+		// Methods
+		public override string ToString()
+		{
+			return $"Is a ground block? {isGroundBlock} Diamond color? {myColor}";
+		}
+
+		// Constants
+		public enum DiamondColor
+		{
+			PURPLE = 0,
+			UNKNOWN,
+		}
+
+		public Color PURPLE_COLOR = Color.FromArgb(251, 56, 251);
+	}
+}
--- DiamondDiggerBot/DiamondDiggerBot/Form1.cs
+++ DiamondDiggerBot/DiamondDiggerBot/Form1.cs
@@ -22,11 +22,12 @@ namespace DiamondDiggerBot
 		{
 			Console.WriteLine("hello world");
 
-			Bitmap testGem = (Bitmap)Image.FromFile("C:\\Users\\peter\\Documents\\DiamondDiggerBot\\DiamondDiggerBot\\DiamondDiggerBot\\TestImages\\PurpleGroundBlock.png");
-			Console.WriteLine(testGem);
 
-			Console.WriteLine("Width = " + testGem.Width + " height = " + testGem.Height);
+			//Console.WriteLine(testGem);
 
+			//Console.WriteLine("Width = " + testGem.Width + " height = " + testGem.Height);
+
+			/*
 			for (int xCord = 0; xCord < testGem.Width; xCord++)
 			{
 				for (int yCord = 0; yCord < testGem.Height; yCord++)
@@ -35,6 +36,45 @@ namespace DiamondDiggerBot
 
 				}
 			}
+			*/
+
+			Bitmap testGem = (Bitmap)Image.FromFile("C:\\Users\\peter\\Documents\\DiamondDiggerBot\\DiamondDiggerBot\\DiamondDiggerBot\\TestImages\\PurpleGroundBlock.png");
+			Color purpleGem = FindAverageColor(new Point(0, 0), testGem.Width, testGem.Height, testGem);
+			//Console.WriteLine(purpleGem);
+			Diamond d = new Diamond(purpleGem);
+			Console.WriteLine(d);
+
+			Bitmap testGem2 = (Bitmap)Image.FromFile("C:\\Users\\peter\\Documents\\DiamondDiggerBot\\DiamondDiggerBot\\DiamondDiggerBot\\TestImages\\GreenWaterBlock.png");
+			Color greenGem = FindAverageColor(new Point(0, 0), testGem2.Width, testGem2.Height, testGem2);
+			//Console.WriteLine(purpleGem);
+			Diamond d2 = new Diamond(greenGem);
+			Console.WriteLine(d2);
+		}
+
+		public static Color FindAverageColor(Point corner, int GEM_WIDTH, int GEM_HEIGHT, Bitmap bm)
+		{
+			int rSum = 0;
+			int gSum = 0;
+			int bSum = 0;
+			int numColors = 36; // precalced for speed, needs to change if the bounds change
+
+			int lowerBoundX = (GEM_WIDTH / 2) - 3;
+			int upperBoundX = (GEM_WIDTH / 2) + 3;
+
+			int lowerBoundY = (GEM_HEIGHT / 2) - 3;
+			int upperBoundY = (GEM_HEIGHT / 2) + 3;
+
+			for (int x = lowerBoundX; x < upperBoundX; x++)
+			{
+				for (int y = lowerBoundY; y < upperBoundY; y++)
+				{
+					Color pixel = bm.GetPixel(corner.X + x, corner.Y + y);
+					rSum += pixel.R;
+					gSum += pixel.G;
+					bSum += pixel.B;
+				}
+			}
+			return Color.FromArgb(rSum / numColors, gSum / numColors, bSum / numColors);
 		}
 	}
 }
